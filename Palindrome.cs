9cbc916a1ebc6f8f8c2a99044b2f89f5c43f653c
Krishaveni RL <krishnaveni.rl@gmail.com>
2020-09-18 17:00:56 -0500

    intial commit

---
+++ Palindrome.cs
@@ -0,0 +1,9 @@
+using System;
+class Palindrome
+{
+    public Boolean isPalindrome(string word)
+    {
+
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace puzzle_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Palindrome palindrome = new Palindrome();
+            var p = palindrome.isPalindrome("Liril");
+            Console.WriteLine("Is Palindrome :" + p);
+        }
+    }
+}
